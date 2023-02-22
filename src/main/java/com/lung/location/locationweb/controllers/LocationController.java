5796ce1ac692d5e4efc8f4e2fb93ba4872672138
Lungdjos <lungudjos@gmail.com>
2023-02-22 12:56:25 +0200

    updating the controller and the app.prop files

--- src/main/java/com/lung/location/locationweb/controllers/LocationController.java
+++ src/main/java/com/lung/location/locationweb/controllers/LocationController.java
@@ -1,4 +1,14 @@
 package com.lung.location.locationweb.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class LocationController {
+    // method that returns a jsp page
+    @RequestMapping("/showCreate")
+    String showCreate(){
+
+        return "createLocation";
+    }
 }
