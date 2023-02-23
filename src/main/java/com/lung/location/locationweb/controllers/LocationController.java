7bff18c25080cae67d847cf5f8fcff9408a51833
Lungdjos <lungudjos@gmail.com>
2023-02-23 09:33:20 +0200

    Rectifying the dependecies

--- src/main/java/com/lung/location/locationweb/controllers/LocationController.java
+++ src/main/java/com/lung/location/locationweb/controllers/LocationController.java
@@ -1,14 +1,13 @@
 package com.lung.location.locationweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("")
 public class LocationController {
     // method that returns a jsp page
-    @RequestMapping("/showCreate")
-    String showCreate(){
-
-        return "createLocation";
-    }
+    @GetMapping("/createLocation")
+    public String showCreate(){return "createLocation";}
 }
